f251d0d87fd15e8275b4c0bec75d901e6aee0c28
GuenoleK <kikabouguenole@gmail.com>
2019-04-20 19:40:04 +0200

    Updating insertions

--- src/main/resources/database/V0.0.1/01_inserts.sql
+++ src/main/resources/database/V0.0.1/01_inserts.sql
@@ -4,6 +4,14 @@ insert into structure (id, name, owner_id) values
 insert into article (id, title, description, content, creation_date, edition_date, user_id) values
 (0, 'wtr10-2b_f', 'wtr10-2b_f description', 'It is the first content here', now(), now(), 3);
 
+insert into role (id, name) values
+(0, 'ADMIN'),
+(1, 'EDITOR'),
+(2, 'VIEWER')
+
+insert into user_role_structure (id, user_id, role_id, structure_id) values
+(0, 3, 0, 0);
+
 insert into article_media_type (id, code) values
 (0, 'PDF'),
 (1, 'AUDIO'),
