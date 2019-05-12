8fd23b5bff8984b57ef20e0a66ec89ca2148a5e2
GuenoleK <kikabouguenole@gmail.com>
2019-05-12 15:58:14 +0200

    Updating the article detail page

--- src/main/webapp/app/views/artcile/article-audio-list.tsx
+++ src/main/webapp/app/views/artcile/article-audio-list.tsx
@@ -3,30 +3,27 @@ import * as ArticleMediaInterface from 'app/shared/model/article-media.model';
 import { observer } from 'mobx-react';
 import { AudioCard } from 'app/components/article/audio-card/audio-card';
 import { articleMediaUtils } from 'app/utils/ArticleMediaUtils';
+import './article-audio-list.scss';
 
 type IArticleMedia = ArticleMediaInterface.IArticleMedia;
 
 @observer
 export class AudioCardList extends React.Component<{ audioList: IArticleMedia[] }> {
   render() {
-    return <div className="audio-list">{this.renderAudioList()}</div>;
+    return <div data-component="audio-list">{this.renderAudioList()}</div>;
   }
 
-  renderAudioList = () => (
-    <div>
-      {this.audioList &&
-        this.audioList.length > 0 &&
-        this.audioList.map(audio => <AudioCard key={`audio-card-${audio.id}`} name={articleMediaUtils.processArticleMediaName(audio)} />)}
-      {this.renderEmptyAudioList()}
-    </div>
-  );
-
-  renderEmptyAudioList = () => {
+  renderAudioList = () => {
     const audioCardList = [];
+    if (this.audioList && this.audioList.length > 0) {
+      this.audioList.forEach(audio =>
+        audioCardList.push(<AudioCard key={`audio-card-${audio.id}`} name={articleMediaUtils.processArticleMediaName(audio)} />)
+      );
+    }
     for (let i = 0; i < 4 - this.audioList.length; i++) {
       audioCardList.push(<AudioCard key={`empty-audio-card-${i}`} name="Audio à uploader" />);
     }
-    return <div>{audioCardList}</div>;
+    return <div className="list">{audioCardList}</div>;
   };
 
   get audioList() {
